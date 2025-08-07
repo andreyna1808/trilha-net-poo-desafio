2ae5e0020a2bbda68febf4cdcfcdbbcfc4144598
Andreyna Carvalho <andreyna.m.carvalho@gmail.com>
2025-08-06 21:23:05 -0400

    challenge c# finished

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("SmartPhone Nokia: ");
+Smartphone nokia = new Nokia("491234", "Modelo 01", "487364378", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("SmartPhone Nokia: ");
+Smartphone iphone = new Iphone("12345", "Modelo 02", "111111111", 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
